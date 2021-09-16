7e01bf99e4cbaaee6fb9c0068909fd07a4f9c695
peter <niceguy0825@gmail.com>
2021-09-16 16:14:42 +0900

    ex00 - log4j 추가.

--- src/test/java/org/zerock/persistence/JDBCTests.java
+++ src/test/java/org/zerock/persistence/JDBCTests.java
@@ -1,9 +1,12 @@
 package org.zerock.persistence;
 
+import static org.junit.Assert.fail;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import org.junit.Test;
+import lombok.extern.log4j.Log4j;
 
+@Log4j
 public class JDBCTests {
  
     static{
@@ -22,9 +25,9 @@ public class JDBCTests {
                 "c##book_ex", 
                 "book_ex")) {
             
-           
+           log.info(con);
         }catch(Exception e){
-            
+            fail(e.getMessage());
         }
     }
 }
