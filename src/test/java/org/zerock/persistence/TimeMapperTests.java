0be47f50e2ddffc055f5067baae898f446b192d5
peter <niceguy0825@gmail.com>
2021-09-17 15:07:55 +0900

    ex00 - Mapper 테스트 2
    xml를 이용하여, MyBatis의 어노테이션을 대신 함.

--- src/test/java/org/zerock/persistence/TimeMapperTests.java
+++ src/test/java/org/zerock/persistence/TimeMapperTests.java
@@ -24,5 +24,12 @@ public class TimeMapperTests {
     	log.info(timeMapper.getClass().getName());
     	log.info(timeMapper.getTime());
     }
+    
+    @Test
+    public void testGetTime2() {
+        log.info("getTime2");
+        log.info(timeMapper.getTime2());
+    
+    }
 
 }
