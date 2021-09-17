e08adb04e4dc5502767ea5b1f0dacc3356d99830
peter <niceguy0825@gmail.com>
2021-09-17 14:28:09 +0900

    ex00 - SQLSessionFactory 추가

--- src/test/java/org/zerock/persistence/DataSourceTests.java
+++ src/test/java/org/zerock/persistence/DataSourceTests.java
@@ -3,6 +3,9 @@ package org.zerock.persistence;
 import static org.junit.Assert.fail;
 import java.sql.Connection;
 import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -21,12 +24,16 @@ public class DataSourceTests {
     @Setter(onMethod_ = {@Autowired})
     private DataSource dataSource;
     
+    @Setter(onMethod_ = {@Autowired})
+    private SqlSessionFactory sqlSessionFactory;
+    
     @Test
-    public void testConnection() {
-        try (Connection con= dataSource.getConnection()){
-            
+    public void testMyBatis() {
+        try (SqlSession session = sqlSessionFactory.openSession();
+    		Connection con = session.getConnection();
+		) {       
+            log.info(session);
             log.info(con);
-            
         } catch (Exception e) {
             fail(e.getMessage());
             
