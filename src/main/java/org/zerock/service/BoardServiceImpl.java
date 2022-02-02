c0db553f01c711f7cf09b594407cc0f477e499d9
peter <niceguy0825@gmail.com>
2022-02-02 23:10:23 +0900

    9.2 비지니스 계층의 구현과 테스트

--- src/main/java/org/zerock/service/BoardServiceImpl.java
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -1,6 +1,9 @@
 package org.zerock.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
 import org.zerock.mapper.BoardMapper;
 
 import lombok.RequiredArgsConstructor;
@@ -13,4 +16,35 @@ public class BoardServiceImpl implements BoardService {
 	// 인터페이스 쓰는 목적은 실제 객체가 뭔지 모르게 한다. 타입만 보고 코딩 할 수 있게 함.
 	
 	private final BoardMapper mapper;
+
+	@Override
+	public Long register(BoardVO board) {
+		mapper.insertSelectKey(board);
+		
+		return board.getBno();
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		
+		return mapper.read(bno);
+	}
+
+	@Override
+	public int modify(BoardVO board) {
+		
+		return mapper.update(board);
+	}
+
+	@Override
+	public int remove(Long bno) {
+		
+		return mapper.delete(bno);
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		
+		return mapper.getList();
+	}
 }
--- src/test/java/org/zerock/service/BoardServiceTests.java
+++ src/test/java/org/zerock/service/BoardServiceTests.java
@@ -6,7 +6,10 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.zerock.domain.BoardVO;
+
 import lombok.extern.log4j.Log4j;
+import sun.rmi.runtime.Log;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration("file:src/main/webapp/WEB-INF/spring/root-context.xml")
@@ -19,6 +22,49 @@ public class BoardServiceTests {
 	@Test
 	public void testPrint() {
 		log.info("service : " + service.toString());
+	}
+	
+	@Test
+	public void testGetList() {
+		service.getList().forEach(board -> log.info(board));
+	}
+	
+	@Test
+	public void testRegister() {
+		BoardVO vo = new BoardVO();
+		vo.setTitle("비지니스 영역");
+		vo.setContent("저항과 지지 라인 잡기");
+		vo.setWriter("Burger");
+		
+		long bno = service.register(vo);
+		log.info("생성된 게시물의 번호 : " + bno);
+	}
+
+	@Test
+	public void testGet() {
+
+		log.info(service.get(1L));
+	}
+
+	@Test
+	public void testDelete() {
+
+		// 게시물 번호의 존재 여부를 확인하고 테스트할 것
+		log.info("REMOVE RESULT: " + service.remove(2L));
+
+	}
+
+	@Test
+	public void testUpdate() {
+
+		BoardVO board = service.get(1L);
+
+		if (board == null) {
+			return;
+		}
+
+		board.setTitle("제목 수정합니다.");
+		log.info("MODIFY RESULT: " + service.modify(board));
 	}
 
 }
