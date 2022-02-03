828525ffe8c1ba7000b729e0a5ced47ff7f10d2a
peter <niceguy0825@gmail.com>
2022-02-03 22:13:08 +0900

    10.2.1 BoardController 목록에 대한 처리와 테스트

---
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -0,0 +1,25 @@
+package org.zerock.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.zerock.service.BoardService;
+
+import lombok.AllArgsConstructor;
+
+@Controller
+@RequestMapping("/board/*")
+@AllArgsConstructor
+public class BoardController {
+	
+	private final BoardService service;
+	
+	@GetMapping("/list")
+	public void list(Model model) {
+		System.out.print("list...........");
+		
+		model.addAttribute("list", service.getList());
+	}
+
+}
