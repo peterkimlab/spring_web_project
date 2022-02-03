44b8163922b79a60ed1d602fa71fd05fccd97eee
peter <niceguy0825@gmail.com>
2022-02-03 22:52:50 +0900

    10.2.3 조회, 삭제 처리와 테스트

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -5,6 +5,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.domain.BoardVO;
 import org.zerock.service.BoardService;
 
@@ -25,12 +27,39 @@ public class BoardController {
 	}
 	
 	@PostMapping("/register")
-	public void register(BoardVO board) {
+	public String register(BoardVO board, RedirectAttributes rttr) {
 		System.out.print("board : " + board);
 		
 		Long bno = service.register(board);
 		
 		System.out.print("BNO : " + bno);
+		rttr.addFlashAttribute("result", bno); // 한번만 전송 되고, 브라우저 창에는 남지 않음
+		
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/modify")
+	public String modify(BoardVO board, RedirectAttributes rttr) {
+		
+		int count = service.modify(board);
+		
+		if (count == 1) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+		return "redirect:/board/list";
+	}
+	
+	@PostMapping("/remove")
+	public String remove(@RequestParam("bno") Long bno, RedirectAttributes rttr) {
+		
+		int count = service.remove(bno);
+		
+		if (count == 1) {
+			rttr.addFlashAttribute("result", "success");
+		}
+		
+		return "redirect:/board/list";
 	}
 
 }
--- src/test/java/org/zerock/controller/BoardControllerTests.java
+++ src/test/java/org/zerock/controller/BoardControllerTests.java
@@ -57,5 +57,33 @@ public class BoardControllerTests {
 						)
 				.andReturn());
 	}
+	
+	@Test
+	public void tetGet() throws Exception {
+
+		log.info(mockMvc.perform(MockMvcRequestBuilders.get("/board/get").param("bno", "1")).andReturn()
+				.getModelAndView().getModelMap());
+	}
+
+	@Test
+	public void testModify() throws Exception {
+
+		String resultPage = mockMvc
+				.perform(MockMvcRequestBuilders.post("/board/modify").param("bno", "1").param("title", "수정된 테스트 새글 제목")
+						.param("content", "수정된 테스트 새글 내용").param("writer", "user00"))
+				.andReturn().getModelAndView().getViewName();
+
+		log.info(resultPage);
+
+	}
+
+	@Test
+	public void testRemove() throws Exception {
+		// 삭제전 데이터베이스에 게시물 번호 확인할 것
+		String resultPage = mockMvc.perform(MockMvcRequestBuilders.post("/board/remove").param("bno", "25")).andReturn()
+				.getModelAndView().getViewName();
+
+		log.info(resultPage);
+	}
 
 }
