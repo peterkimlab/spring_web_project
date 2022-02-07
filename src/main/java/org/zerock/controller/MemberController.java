d3a1c37276ccd9b45c5b626dbe2818bf1a84f9e1
peter <niceguy0825@gmail.com>
2022-02-07 13:23:07 +0900

    [Spring][쇼핑몰 프로젝트][8] 로그인 기능 구현(페이지,메서드)  (2)

--- src/main/java/org/zerock/controller/MemberController.java
+++ src/main/java/org/zerock/controller/MemberController.java
@@ -1,5 +1,8 @@
 package org.zerock.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.model.MemberVO;
 import org.zerock.service.MemberService;
 
@@ -68,5 +72,30 @@ public class MemberController {
 		}
 		
 	} // memberIdChkPOST() 종료
+	
+	/* 로그인 */
+	@RequestMapping(value="login", method=RequestMethod.POST)
+	public String loginPOST(HttpServletRequest request, MemberVO member, RedirectAttributes rttr) throws Exception{
+		
+		//System.out.println("login 메서드 진입");
+		//System.out.println("전달된 데이터 : " + member);
+		
+		HttpSession session = request.getSession();
+		MemberVO lvo = memberservice.memberLogin(member);
+		
+		System.out.println("lvo : " + lvo);
+		
+		if(lvo == null) {								// 일치하지 않는 아이디, 비밀번호 입력 경우
+			
+			int result = 0;
+			rttr.addFlashAttribute("result", result);
+			return "redirect:/member/login";
+			
+		}
+		
+		session.setAttribute("member", lvo); 			// 일치하는 아이디, 비밀번호 경우 (로그인 성공)
+		
+		return "redirect:/main";
+	}
 	
 }
