0b0335b9ba3ea3d17f26297e0619cb96c24ca84a
peter <niceguy0825@gmail.com>
2022-02-07 12:48:32 +0900

    [Spring][쇼핑몰 프로젝트][8] 로그인 기능 구현(쿼리문, mapper,service)

--- src/test/java/org/zerock/mapper/MemberMapperTests.java
+++ src/test/java/org/zerock/mapper/MemberMapperTests.java
@@ -80,6 +80,25 @@ public class MemberMapperTests {
 		membermapper.idCheck(id2);
 	}
 	
+	/* 로그인 쿼리 mapper 메서드 테스트 */
+    @Test
+    public void memberLogin() throws Exception{
+        
+        MemberVO member = new MemberVO();    // MemberVO 변수 선언 및 초기화
+        
+        /* 올바른 아이디 비번 입력경우 */
+        member.setMemberId("test");
+        member.setMemberPw("test");
+        
+        /* 올바른 않은 아이디 비번 입력경우 */
+        //member.setMemberId("test1123");
+        //member.setMemberPw("test1321321");
+        
+        membermapper.memberLogin(member);
+        System.out.println("결과 값 : " + membermapper.memberLogin(member));
+        
+    }
+	
 	
 	
 }
