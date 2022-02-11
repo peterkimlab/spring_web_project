1a61ffe264e4f4f0da14ce168544e02d04508682
peter <niceguy0825@gmail.com>
2022-02-11 20:39:45 +0900

    [Spring][쇼핑몰 프로젝트][16] 작가 상세 페이지
    작가 상세 페이지(authorDetail.jsp) 구현

--- src/main/java/org/zerock/service/AuthorService.java
+++ src/main/java/org/zerock/service/AuthorService.java
@@ -14,6 +14,9 @@ public interface AuthorService {
     public List<AuthorVO> authorGetList(Criteria cri) throws Exception;
     
     /* 작가 총 수 */
-    public int authorGetTotal(Criteria cri) throws Exception;   
+    public int authorGetTotal(Criteria cri) throws Exception;
+    
+    /* 작가 상세 페이지 */
+	public AuthorVO authorGetDetail(int authorId) throws Exception;
  	
 }
--- src/test/java/org/zerock/mapper/AuthorMapperTests.java
+++ src/test/java/org/zerock/mapper/AuthorMapperTests.java
@@ -86,4 +86,16 @@ public class AuthorMapperTests {
 		
 		
 	}
+	
+	/* 작가 상세 페이지 */
+	@Test
+	public void authorGetDetailTest() {
+		
+		int authorId = 30;
+		
+		AuthorVO author = mapper.authorGetDetail(authorId);
+		
+		System.out.println("author......." + author);
+		
+	}
 }
