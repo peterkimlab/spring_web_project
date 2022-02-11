b0550bdf60b5f45a37a9f7c22990a1a3d05a6b4a
peter <niceguy0825@gmail.com>
2022-02-12 02:55:17 +0900

    [Spring][쇼핑몰 프로젝트][17] 작가 수정 페이지 - 2
    작가 정보 수정 기능 구현

--- src/main/java/org/zerock/controller/AdminController.java
+++ src/main/java/org/zerock/controller/AdminController.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -60,7 +61,7 @@ public class AdminController {
         /* 작가 목록 출력 데이터 */
         List list = authorService.authorGetList(cri);
         
-        System.out.println("authorManage :::::: " + list);
+        System.out.println("authorManage ::::::: " + list);
         
         logger.info("authorManage -------- " +  list);
         
@@ -107,6 +108,21 @@ public class AdminController {
 		
 		/* 선택 작가 정보 */
 		model.addAttribute("authorInfo", authorService.authorGetDetail(authorId));
+		
+	}
+    
+    /* 작가 정보 수정 */
+	@PostMapping("/authorModify")
+	public String authorModifyPOST(AuthorVO author, RedirectAttributes rttr) throws Exception{
+		
+		logger.info("authorModifyPOST......." + author);
+		System.out.println("authorModifyPOST ::::::: " + author);
+		
+		int result = authorService.authorModify(author);
+		
+		rttr.addFlashAttribute("modify_result", result);
+		
+		return "redirect:/admin/authorManage";
 		
 	}
 	
