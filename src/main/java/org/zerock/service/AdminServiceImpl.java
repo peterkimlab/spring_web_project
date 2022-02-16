99ccfa3baf4a9f4c136704033a0cf3df0753f984
peter <niceguy0825@gmail.com>
2022-02-17 02:26:04 +0900

    [Spring][쇼핑몰 프로젝트][27] 업로드 이미지 정보 등록 - 5 (트랜잭션 적용)
    1. 트랜잭션 설정
    2. 트랜잭션 어노테이션 적용

--- src/main/java/org/zerock/service/AdminServiceImpl.java
+++ src/main/java/org/zerock/service/AdminServiceImpl.java
@@ -6,7 +6,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mapper.AdminMapper;
 import org.zerock.model.BookVO;
 import org.zerock.model.CateVO;
@@ -23,6 +23,7 @@ public class AdminServiceImpl implements AdminService {
 	private AdminMapper adminMapper;	
 	
 	/* 상품 등록 */
+	@Transactional
 	@Override
 	public void bookEnroll(BookVO book) {
 		
