00a1148e3d3a6b16346f4405b6ddbd4c803f0d71
peter <niceguy0825@gmail.com>
2022-02-17 15:56:15 +0900

    [Spring][쇼핑몰 프로젝트][29] 상품 이미지 수정 - 4(배치 프로그램 적용 1)
    배치 프로그램 이해 및 적용을 위한 환경설정

---
+++ src/main/java/org/zerock/task/testBatch.java
@@ -0,0 +1,18 @@
+package org.zerock.task;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class testBatch {
+
+
+	@Scheduled(cron = "0 * * * * *")
+	public void testMethod() throws Exception{
+		
+		System.out.println("배치 실행 테스트.......");
+		System.out.println("===================================");
+		
+	}
+	
+}
