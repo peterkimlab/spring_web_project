b45265deed231be184e094fab307e30c420eebf9
peter <niceguy0825@gmail.com>
2022-02-19 17:11:21 +0900

    [Spring][쇼핑몰 프로젝트][31] 상품 목록 화면 구현 - 1
    1. 메인화면(main.jsp) css 설정 추가
    2. 상품 목록 화면(search.jsp) 
     1) 기본 틀
     2) 검색 결과 유무

--- src/main/java/org/zerock/controller/BookController.java
+++ src/main/java/org/zerock/controller/BookController.java
@@ -79,7 +79,7 @@ public class BookController {
 	}
 	
 	/* 상품 검색 */
-	@GetMapping("search")
+	@GetMapping("/search")
 	public String searchGoodsGET(Criteria cri, Model model) {
 		
 		logger.info("cri : " + cri);
