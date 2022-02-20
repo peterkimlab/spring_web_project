70c0f74b2a8b4e556132a02f6d9db42a13ec05aa
peter <niceguy0825@gmail.com>
2022-02-20 16:20:12 +0900

    [Spring][쇼핑몰 프로젝트][33] 메인화면 네비 기능
    1. 네비 태그(main.jsp)
    2. 카테고리 <a>태그

--- src/main/java/org/zerock/task/AttachFileCheckTask.java
+++ src/main/java/org/zerock/task/AttachFileCheckTask.java
@@ -39,51 +39,51 @@ public class AttachFileCheckTask {
 	@Scheduled(cron="0 * * * * *")
 	public void checkFiles() throws Exception{	
 		
-		System.out.println("File Check Task Run..........");
-		System.out.println(new Date());
-		System.out.println("========================================");		
-		
-		// DB에 저장된 파일 리스트
-		List<AttachImageVO> fileList = mapper.checkFileList();		
-		
-		
-		// 비교 기준 파일 리스트(Path객체)
-		List<Path> checkFilePath = new ArrayList<Path>();
-			//원본 이미지
-		fileList.forEach(vo -> {
-			Path path = Paths.get("/Users/peter/workspace/spring_workspace/image_folder/", vo.getUploadPath(), vo.getUuid() + "_" + vo.getFileName());
-			checkFilePath.add(path);
-		});		
-			//썸네일 이미지
-		fileList.forEach(vo -> {
-			Path path = Paths.get("/Users/peter/workspace/spring_workspace/image_folder/", vo.getUploadPath(), "s_" +  vo.getUuid() + "_" + vo.getFileName());
-			checkFilePath.add(path);
-		});
-		
-		
-		// 디렉토리 파일 리스트
-		File targetDir = Paths.get("/Users/peter/workspace/spring_workspace/image_folder/", getFolderYesterDay()).toFile();
-		File[] targetFile = targetDir.listFiles();
-		
-		
-		// 삭제 대상 파일 리스트(분류)
-		List<File> removeFileList = new ArrayList<File>(Arrays.asList(targetFile));		
-		for(File file : targetFile){
-			checkFilePath.forEach(checkFile ->{
-				if(file.toPath().equals(checkFile)) 
-					removeFileList.remove(file);	
-			});
-		}
-		
-		
-		// 삭제 대상 파일 제거
-		System.out.println("file Delete : ");
-		for(File file : removeFileList) {
-			System.out.println(file);
-			file.delete();
-		}		
-		
-		System.out.println("========================================");
+//		System.out.println("File Check Task Run..........");
+//		System.out.println(new Date());
+//		System.out.println("========================================");		
+//		
+//		// DB에 저장된 파일 리스트
+//		List<AttachImageVO> fileList = mapper.checkFileList();		
+//		
+//		
+//		// 비교 기준 파일 리스트(Path객체)
+//		List<Path> checkFilePath = new ArrayList<Path>();
+//			//원본 이미지
+//		fileList.forEach(vo -> {
+//			Path path = Paths.get("/Users/peter/workspace/spring_workspace/image_folder/", vo.getUploadPath(), vo.getUuid() + "_" + vo.getFileName());
+//			checkFilePath.add(path);
+//		});		
+//			//썸네일 이미지
+//		fileList.forEach(vo -> {
+//			Path path = Paths.get("/Users/peter/workspace/spring_workspace/image_folder/", vo.getUploadPath(), "s_" +  vo.getUuid() + "_" + vo.getFileName());
+//			checkFilePath.add(path);
+//		});
+//		
+//		
+//		// 디렉토리 파일 리스트
+//		File targetDir = Paths.get("/Users/peter/workspace/spring_workspace/image_folder/", getFolderYesterDay()).toFile();
+//		File[] targetFile = targetDir.listFiles();
+//		
+//		
+//		// 삭제 대상 파일 리스트(분류)
+//		List<File> removeFileList = new ArrayList<File>(Arrays.asList(targetFile));		
+//		for(File file : targetFile){
+//			checkFilePath.forEach(checkFile ->{
+//				if(file.toPath().equals(checkFile)) 
+//					removeFileList.remove(file);	
+//			});
+//		}
+//		
+//		
+//		// 삭제 대상 파일 제거
+//		System.out.println("file Delete : ");
+//		for(File file : removeFileList) {
+//			System.out.println(file);
+//			file.delete();
+//		}		
+//		
+//		System.out.println("========================================");
 		
 	}
 	
