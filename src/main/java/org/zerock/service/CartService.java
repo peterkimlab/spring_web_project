b98f2c08534ed090e26bc0e9f0eb9b214b9162cb
peter <niceguy0825@gmail.com>
2022-02-22 01:11:57 +0900

    [Spring][쇼핑몰 프로젝트][37] 장바구니 기능(장바구니 페이지) -1
    1. 포인트 정보 추가
    2. CartService.java
    3. CartController.java
    4. cart.jsp

--- src/main/java/org/zerock/service/CartService.java
+++ src/main/java/org/zerock/service/CartService.java
@@ -1,10 +1,15 @@
 package org.zerock.service;
 
+import java.util.List;
+
 import org.zerock.model.CartDTO;
 
 public interface CartService {
 	
 	/* 장바구니 추가 */
-	public int addCart(CartDTO cart);	
+	public int addCart(CartDTO cart);
+	
+	/* 장바구니 정보 리스트 */
+	public List<CartDTO> getCartList(String memberId);
 
 }
