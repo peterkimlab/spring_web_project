57dbbc8f3f7d7ecb3af41f5ce08b8b05efabfc61
peter <niceguy0825@gmail.com>
2022-02-23 01:19:01 +0900

    [Spring][쇼핑몰 프로젝트][41] 주문 구현(주문 페이지) - 6
    '주문 페이지' 상품란에 이미지를 넣기

--- src/main/java/org/zerock/service/OrderServiceImpl.java
+++ src/main/java/org/zerock/service/OrderServiceImpl.java
@@ -5,8 +5,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import org.zerock.mapper.AttachMapper;
 import org.zerock.mapper.OrderMapper;
+import org.zerock.model.AttachImageVO;
 import org.zerock.model.OrderPageItemDTO;
 
 @Service
@@ -14,6 +15,9 @@ public class OrderServiceImpl implements OrderService{
 
 	@Autowired
 	private OrderMapper orderMapper;
+	
+	@Autowired
+	private AttachMapper attachMapper;
 
 	@Override
 	public List<OrderPageItemDTO> getGoodsInfo(List<OrderPageItemDTO> orders) {
@@ -26,7 +30,11 @@ public class OrderServiceImpl implements OrderService{
 
 			goodsInfo.setBookCount(ord.getBookCount());	
 			
-			goodsInfo.initSaleTotal();			
+			goodsInfo.initSaleTotal();
+			
+			List<AttachImageVO> imageList = attachMapper.getAttachList(goodsInfo.getBookId());
+			
+			goodsInfo.setImageList(imageList);			
 			
 			result.add(goodsInfo);			
 		}		
