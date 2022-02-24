d0d524b01a6e642df3633967ea01ff30aa043884
peter <niceguy0825@gmail.com>
2022-02-24 18:22:42 +0900

    [Spring][쇼핑몰 프로젝트][47] 댓글 페이징 - 2
    서버로부터 반환받은 댓글 페이지 정보를 활용해서 동적으로 태그 생성

--- src/main/java/org/zerock/controller/ReplyController.java
+++ src/main/java/org/zerock/controller/ReplyController.java
@@ -1,11 +1,14 @@
 package org.zerock.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.zerock.model.Criteria;
 import org.zerock.model.ReplyDTO;
+import org.zerock.model.ReplyPageDTO;
 import org.zerock.service.ReplyService;
 
 
--- src/test/java/org/zerock/mapper/ReplyMapperTests.java
+++ src/test/java/org/zerock/mapper/ReplyMapperTests.java
@@ -1,11 +1,13 @@
 package org.zerock.mapper;
 
+import java.util.List;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
+import org.zerock.model.Criteria;
 import org.zerock.model.ReplyDTO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
@@ -35,5 +37,18 @@ public class ReplyMapperTests {
 		
 	}
 	
+	@Test
+	public void replyListTest() {
+		
+		Criteria cri = new Criteria(1,10);    // 3페이지 & 10개 행 표시
+        cri.setBookId(1);
+		
+        List<ReplyDTO> list = mapper.getReplyList(cri);
+		
+		for(int i = 0; i < list.size(); i++) {
+            System.out.println("replyList" + i + ".........." + list.get(i));
+        }
+		
+	}
 	
 }
